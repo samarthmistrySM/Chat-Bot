8db25e3d4dfd34c3e9d9ed6a4a0adbe072c30a88
Samarth Mistry <g33ky.sam@gmail.com>
2024-05-21 16:03:12 +0530

    neew

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import ChatApp from "./components/ChatApp.jsx";
 import ImageGen from "./components/ImageGen.jsx";
 
+
 export default function App() {
   const [state, setState] = useState("Text");
 
@@ -12,9 +13,12 @@ export default function App() {
   const handleImageMode = () => {
     setState("Image");
   };
+  
+
 
   return (
     <div className="flex h-screen w-screen justify-center md:items-center relative">
+
       <div className="w-1/3">
         <button className="w-full h-screen" onClick={handleTextMode}>
           Text
